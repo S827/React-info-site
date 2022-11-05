1cde6b26381956cb289ff452ca339a9c1ceada4b
s827 <dskumar827@gmail.com>
2022-11-05 22:15:23 +0530

    updated some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import MainContent from './components/MainContent'
+import Icons from './icons'
 import './index.css'
 
 export default function App() {
   return (
     <div>
       <Navbar />
+      <Icons />
       <MainContent />
     </div>
   )
---
+++ src/icons.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Fa500Px } from "react-icons/fa"
+
+export default function icons() {
+    return (
+        <Fa500Px />
+    )
+}
